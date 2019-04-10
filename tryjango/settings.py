fec29f1235ce752a55c3e8c190917de119b4c0ff
DasSonia <soniadas363@gmail.com>
2019-04-10 17:53:02 +0530

    Inbuilt Apps: Admin and Auth

--- tryjango/settings.py
+++ tryjango/settings.py
@@ -35,12 +35,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.admin', # Admin
+    'django.contrib.auth',  # Admin Auth
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
+    # Own
 ]
 
 MIDDLEWARE = [
