c84696b13d921f10b1be289170594b0e70256164
DasSonia <soniadas363@gmail.com>
2019-04-11 18:22:31 +0530

    Templates

--- pages/views.py
+++ pages/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponse # Make string to html
 def home_view(request, *args, **kwargs): # *args, **kwargs
     print(args, kwargs) # REQUEST
     print(request.user) # USER NAME
-    return HttpResponse("<h1>Hello World!</h1>") # string of HTML code
+    #return HttpResponse("<h1>Hello World!</h1>") # string of HTML code
+    return render(request,"home.html",{})
 
 def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Contact Page</h1>")
