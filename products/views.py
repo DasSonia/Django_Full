06b6435ce6876e05392f7416243218078e5a91a6
DasSonia <soniadas363@gmail.com>
2019-04-15 18:08:25 +0530

    Raw HTML Form

--- products/views.py
+++ products/views.py
@@ -5,15 +5,29 @@ from .models import Product
 from .forms import ProductForm
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm() # RE-RENDER IT 
-
+    # FOR POST  {% csrf_token %} SECURITY IS NEEDED
+    #print(request.GET)
+    #print(request.GET['title'])
+    #print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+        # Product.objects.create(title=my_new_title)    
     context = {
-        'form': form
+        
     }
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm() # RE-RENDER IT 
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
