1e42121be3dd4a90c837c71437482d1b10b8bcba
DasSonia <soniadas363@gmail.com>
2019-04-15 10:45:55 +0530

    Template Tags and Filters

--- pages/views.py
+++ pages/views.py
@@ -19,9 +19,12 @@ def about_view(request, *args, **kwargs):
     #return HttpResponse("<h1>About Page</h1>")
     my_context = {
         "my_text": "This is about us text",
+        "title": "abc this is about us",
         "my_number": 123,
         "this_is_true": True,
-        "my_list": [1313, 4231, 312, "Abc list"]
+        "my_list": [1313, 4231, 312, "Abc list"],
+        "my_html": "<h1>Hello World</h1>"
+        # https://docs.djangoproject.com/en/2.2/ref/templates/builtins/
 
     }
     return render(request, "about.html", my_context)
