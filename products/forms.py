e72d43ca73dcb24bd66f1c2d96b371f9d367fc5a
DasSonia <soniadas363@gmail.com>
2019-04-15 18:32:23 +0530

    Form Widgets

--- products/forms.py
+++ products/forms.py
@@ -12,7 +12,20 @@ class ProductForm(forms.ModelForm):
             'price'
         ] 
 
+
+
 class RawProductForm(forms.Form):
-    title       = forms.CharField()
-    description = forms.CharField()
-    price       = forms.DecimalField() 
+    title       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(
+                        required=False, 
+                        widget=forms.Textarea(
+                                attrs={
+                                    "placeholder": "Your description",
+                                    "class": "new-class-name two",
+                                    "id": "my-id-for-textarea",
+                                    "rows": 20,
+                                    'cols': 120
+                                }
+                            )
+                        )
+    price       = forms.DecimalField(initial=199.99)    
